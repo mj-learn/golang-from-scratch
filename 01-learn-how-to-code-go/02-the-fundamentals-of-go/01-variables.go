43de54674498d469d05b233740a8b50c7ae8c662
MOnjofn <monjofn@gmail.com>
2023-07-18 23:41:08 +0300

    add(lecture): variables

---
+++ 01-learn-how-to-code-go/02-the-fundamentals-of-go/01-variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+  var h int = 42
+  var z = 52
+  fmt.Println(h,z)
+
+  a := 7
+  fmt.Println(a)
+
+  b, c, d, _, f := 1, 2, 3, 4, "string"
+  fmt.Println(b, c, d, f)
+
+  var g int
+  fmt.Println(g)
+  g=15
+  fmt.Println(g)
+}
