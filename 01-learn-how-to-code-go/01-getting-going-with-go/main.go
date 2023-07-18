303a241d934f4fba2714e4f0c27e071fd4f4bc0e
MOnjofn <monjofn@gmail.com>
2023-07-18 21:57:14 +0300

    add(exercise): getting going with go

---
+++ 01-learn-how-to-code-go/01-getting-going-with-go/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("hello world")
+  fmt.Println("hello world")
+
+  fmt.Print("\nhello world")
+  fmt.Print(" hello world\n")
+
+    fmt.Println(`
+    string
+    string 2
+    string 3
+  `)
+
+  // single line comment
+
+  /* 
+  multiline
+  comment
+  */
+}
