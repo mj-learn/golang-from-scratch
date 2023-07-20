7cc1b4d6eb4d0dea2bf85db7681e34abe06d7cb1
MOnjofn <monjofn@gmail.com>
2023-07-20 15:51:33 +0300

    refactor: fix filenames

---
+++ 01-learn-how-to-code-go/03-tour-of-go/03-packages.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+)
+
+func main() {
+  fmt.Println("My favorite number is", rand.Intn(3))
+  fmt.Println("My favorite number is", rand.Intn(3))
+  fmt.Println("My favorite number is", rand.Intn(3))
+  fmt.Println("My favorite number is", rand.Intn(3))
+}
