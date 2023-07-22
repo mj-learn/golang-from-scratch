d0e52857571b1bd650d46ae2595286b8bf1e1bfa
MOnjofn <monjofn@gmail.com>
2023-07-22 22:06:42 +0300

    add(exercise): find modulus aka remainder

---
+++ 01-learn-how-to-code-go/07-control-flow-exercises/01-modulus-remainder/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10 % 3
+	y := 83 % 40
+	z := 83 / 40
+
+	fmt.Println("10 % 3 =", x)
+	fmt.Println("83 % 40 =", y)
+	fmt.Println("83 / 40 =", z)
+}
