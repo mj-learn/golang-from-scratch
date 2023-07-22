823a7db17ca55f796ca15a7ae91999e6c317c81e
MOnjofn <monjofn@gmail.com>
2023-07-22 17:41:37 +0300

    style: fix identation

--- 01-learn-how-to-code-go/05-mod-and-dependency/main.go
+++ 01-learn-how-to-code-go/05-mod-and-dependency/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-  "05-mod-and-dependency/mypackage"
-  "05-mod-and-dependency/mypackage/mysubpackage"
-  "05-mod-and-dependency/mypackage/mysubpackage/subsub"
-  "fmt"
+	"fmt"
 
-  "github.com/monjofn/puppy"
+	"github.com/monjofn/puppy"
+
+	"05-mod-and-dependency/mypackage"
+	"05-mod-and-dependency/mypackage/mysubpackage"
+	"05-mod-and-dependency/mypackage/mysubpackage/subsub"
 )
 
-
 func main() {
-  fmt.Println("Hello from main")
-  puppy.Bark()
-  puppy.BarkBark()
-  mypackage.PrintHello()
-  mysubpackage.HelloFromMySubPackage()
-  subsub.HelloFromSubSub()
+	fmt.Println("Hello from main")
+	puppy.Bark()
+	puppy.BarkBark()
+	mypackage.PrintHello()
+	mysubpackage.HelloFromMySubPackage()
+	subsub.HelloFromSubSub()
 }
