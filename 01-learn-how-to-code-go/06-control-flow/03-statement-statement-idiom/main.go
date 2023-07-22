7afab27026ad4ce212e113e54341b2803ced0f77
MOnjofn <monjofn@gmail.com>
2023-07-22 17:42:22 +0300

    style: fix identation

--- 01-learn-how-to-code-go/06-control-flow/03-statement-statement-idiom/main.go
+++ 01-learn-how-to-code-go/06-control-flow/03-statement-statement-idiom/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-  "fmt"
-  "math/rand"
+	"fmt"
+	"math/rand"
 )
 
 func main() {
+	x := 5
+	y := x + 5
+	fmt.Printf("y is %v + 5 = %v\n", x, y)
 
-  x := 5; y := x + 5
-  fmt.Printf("y is %v + 5 = %v\n", x, y)
+	fmt.Println()
 
-  fmt.Println()
-  
-  // Statement, statement ideom
-  // if z:=5; foo := z * rand.Intin(z); foo >= z  -> this will get error
+	// Statement, statement ideom
+	// if z:=5; foo := z * rand.Intin(z); foo >= z  -> this will get error
 
-  if foo := x * rand.Intn(x); foo >= x {
-    fmt.Printf("foo is %v and is GREATER OR EQUAL to x which is %v", foo, x)
-  } else {
-    fmt.Printf("foo is %v and is LESS THAN x which is %v", foo, x)
-  }
+	if foo := x * rand.Intn(x); foo >= x {
+		fmt.Printf("foo is %v and is GREATER OR EQUAL to x which is %v", foo, x)
+	} else {
+		fmt.Printf("foo is %v and is LESS THAN x which is %v", foo, x)
+	}
 
-  fmt.Println()
+	fmt.Println()
 }
