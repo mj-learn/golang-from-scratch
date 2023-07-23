184f59cc46911ec69984b7304a7a67ebf530f828
MOnjofn <monjofn@gmail.com>
2023-07-23 16:55:25 +0300

    style(exercise): fix identation

--- 01-learn-how-to-code-go/02-the-fundamentals-of-go/01-variables.go
+++ 01-learn-how-to-code-go/02-the-fundamentals-of-go/01-variables.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-  var h int = 42
-  var z = 52
-  fmt.Println(h,z)
+	var h int = 42
+	z := 52
+	fmt.Println(h, z)
 
-  a := 7
-  fmt.Println(a)
+	a := 7
+	fmt.Println(a)
 
-  b, c, d, _, f := 1, 2, 3, 4, "string"
-  fmt.Println(b, c, d, f)
+	b, c, d, _, f := 1, 2, 3, 4, "string"
+	fmt.Println(b, c, d, f)
 
-  var g int
-  fmt.Println(g)
-  g=15
-  fmt.Println(g)
+	var g int
+	fmt.Println(g)
+	g = 15
+	fmt.Println(g)
 }
