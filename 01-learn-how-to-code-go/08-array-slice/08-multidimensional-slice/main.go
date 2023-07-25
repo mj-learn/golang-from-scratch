6a02182c619ee5f8c0a29cc231eaf508ddb0428e
MOnjofn <monjofn@gmail.com>
2023-07-25 17:02:14 +0300

    update(exercise): multidimensional slice

--- 01-learn-how-to-code-go/08-array-slice/08-multidimensional-slice/main.go
+++ 01-learn-how-to-code-go/08-array-slice/08-multidimensional-slice/main.go
@@ -1,4 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
+	jb := []string{"James", "Bond", "Martini", "Chocolate"}
+	jm := []string{"Jenny", "Moneypenny", "Vodka", "Vanila"}
+
+	// Multidimensional slice is slice of slices
+	xxs := [][]string{jb, jm, {"MOn", "MOnjofn", "Water", "Oreo"}}
+	fmt.Println(xxs)
 }
