b9020c08839401f6792f9ee789535f5480b3c5bd
MOnjofn <monjofn@gmail.com>
2023-07-25 18:15:06 +0300

    update(exercise): slice internals and underlying array

--- 01-learn-how-to-code-go/08-array-slice/09-slice-internals/main.go
+++ 01-learn-how-to-code-go/08-array-slice/09-slice-internals/main.go
@@ -1,6 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+func sortOne(s []int) []int {
+	sort.Ints(s)
+	return s
+}
+
+func sortTwo(s []int) []int {
+	n := make([]int, len(s))
+	copy(n, s)
+	sort.Ints(n)
+	return n
+}
 
 func main() {
 	a := "MOn"
@@ -19,14 +34,32 @@ func main() {
 
 	fmt.Println()
 
-	xi := []int{1, 2, 3}
-	yi := xi
+	xi := []int{5, 3, 1}
+	yi := xi             // create pointer to underlying array
+	ci := make([]int, 3) // create new slice
+	ci[0] = 8
+	fmt.Printf("ci = %#v\n", ci)
+	copy(ci, xi) // copy elements from xi in ci
 	fmt.Printf("xi = %#v\n", xi)
 	fmt.Printf("yi = %#v\n", yi)
+	fmt.Printf("ci = %#v\n", ci)
 
 	fmt.Println()
 
 	xi[0] = 7
 	fmt.Printf("xi = %#v\n", xi)
 	fmt.Printf("yi = %#v\n", yi)
+	fmt.Printf("ci = %#v\n", ci)
+
+	fmt.Println()
+
+	fmt.Println("xi (before sortOne) -", xi)
+	fmt.Println("sortOne return:", sortOne(xi))
+	fmt.Println("xi (after sortOne) -", xi)
+
+	fmt.Println()
+
+	fmt.Println("ci (before sortOne) -", ci)
+	fmt.Println("sortTwo return:", sortTwo(ci))
+	fmt.Println("ci (after sortOne) -", ci)
 }
