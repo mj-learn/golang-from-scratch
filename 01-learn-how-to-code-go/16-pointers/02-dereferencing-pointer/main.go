293f9ff5200615454de1cbf25a205e48d8d8a34d
MOnjofn <monjofn@gmail.com>
2023-07-30 14:44:57 +0300

    add(update): dereferencing pointer

--- 01-learn-how-to-code-go/16-pointers/02-dereferencing-pointer/main.go
+++ 01-learn-how-to-code-go/16-pointers/02-dereferencing-pointer/main.go
@@ -6,14 +6,14 @@ func main() {
 	x := "MOn"
 	fmt.Println("x := \"MOn\"")
 	fmt.Println("x ->", x)     // value
-	fmt.Println("&x ->", &x)   // memory adress (path to value in memory)
+	fmt.Println("&x ->", &x)   // pointer -> memory adress (path to value in memory)
 	fmt.Println("*&x ->", *&x) // dereferencing (get the value from memory adress)
 
 	fmt.Println()
 	fmt.Println("y := &x")
 
 	y := &x
-	fmt.Println("y ->", y)   // value
+	fmt.Println("y ->", y)   // pointer to x
 	fmt.Println("*y ->", *y) // dereferencing (get the value from memory adress)
 
 	fmt.Println()
